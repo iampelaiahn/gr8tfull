7df7b6f84e5e2fbae60a98adb739ba79b0dd51fc
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-08-18 17:44:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button data-sideb

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,9 +1,10 @@
+
 "use client"
 
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { PanelLeft } from "lucide-react"
+import { PanelLeft, PanelLeftClose, PanelLeftOpen } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -263,7 +264,7 @@ const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
 >(({ className, onClick, ...props }, ref) => {
-  const { toggleSidebar } = useSidebar()
+  const { toggleSidebar, state } = useSidebar()
 
   return (
     <Button
@@ -278,7 +279,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      {state === 'collapsed' ? <PanelLeftOpen /> : <PanelLeftClose />}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
@@ -761,3 +762,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
