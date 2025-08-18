975861c56413dc3824c9791fdb64a330d8b47307
Tadiwanashe Ngarande (Mufaro foundation) <mufarofoundation@gmail.com>
2025-08-18 17:47:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button data-sideb

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -272,7 +272,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-8 w-8", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
@@ -762,5 +762,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
